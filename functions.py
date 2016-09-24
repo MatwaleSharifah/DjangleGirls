d2797e410717ca32c54fd49b3ff48f15802004f1
sharifah matwale <sharifah matwale>
2016-09-24 11:53:21 +0300

    my first commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+"""def age_limit(a,b):
+    girl_age=a/b+3
+    return girl_age
+mans_limit=age_limit(30,80)
+print("u can date agirl of",mans_limit, "or older")"""
+
+def compute_sum(x,y):
+    total=x+y
+    return total
+    so=compute_sum(6,8)
+    print("the sum is" ,so, "thank you")
+
